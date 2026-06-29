c456bd009dc7aba62b091487596b4d3884c1ba8e
Ashwani Singh <itsashksingh10@gmail.com>
2026-06-29 20:37:15 +0530

    Implement real-time checkbox updates with Socket.IO and add checkbox state retrieval endpoint

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ import express from 'express'
 import { createServer } from 'node:http'
 import { resolve } from 'node:path'
 import 'dotenv/config'
-
+import { Server } from 'socket.io'
 const app = express()
 const server = createServer(app)
 const PORT = process.env.PORT ?? 3001
+const CHECKBOX_COUNT = 10
 
+// IN MEMORY STATE
+const state = {
+    checkboxes : new Array(CHECKBOX_COUNT).fill(false)   
+}
+
+const io = new Server(server, {connectionStateRecovery: {}})
+io.on('connection', (socket)=>{
+    socket.on('client:checkbox:change', (data)=>{  
+        console.log(data) 
+        state.checkboxes[data.index] = data.checked
+        socket.broadcast.emit('change-ui', data)
+    })
+})
 
 app.use(express.static(resolve('./public')))
 app.get('/health', (req, res)=>{
     res.json({message: "Server is healthy"})
+})
+app.get('/checkboxes', (req, res)=> {
+    res.json({checkboxes: state.checkboxes})
 })
 
 server.listen(PORT, ()=>{
--- public/index.js
+++ public/index.js
@@ -1,12 +1,36 @@
 const containerEl = document.getElementById('container')
-const checkbox = document.createElement('input')
-checkbox.type = 'checkbox'
+const socket = io()
+socket.on('change-ui', (data)=>{
+    const checkboxEl = document.getElementById(data.index)
+    checkboxEl.checked = data.checked
+    
+    
+})
 
-const CHECKBOX_COUNT = 1000
-const array = new Array(CHECKBOX_COUNT).fill(null)
-array.forEach((_, index)=>{
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.id = index
+
+window.addEventListener('load', async ()=>{
+    const response = await fetch('/checkboxes')
+    const data = await response.json()
+    
+    const array = data.checkboxes
+    array.forEach((checked, index)=>{
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.id = index
+        checkbox.className = 'checkbox'
+        checkbox.checked = checked
+        checkbox.addEventListener('change', (e)=>{
+            const checked = e.target.checked
+            const data = {index, checked}
+            socket.emit('client:checkbox:change', data)
+        
+
+    })
+
     containerEl.appendChild(checkbox)
 })
+
+})
+
+
+
